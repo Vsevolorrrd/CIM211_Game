829ff5a4d02e4601375431550f501fbe1889cecb
Jan Willem Goedvolk <jan.goedvolk@student.saedubai.com>
2025-08-15 21:02:10 +0400

    Polish Work

--- Assets/Scripts/FakeGame/ShiftManager.cs
+++ Assets/Scripts/FakeGame/ShiftManager.cs
@@ -88,12 +88,14 @@ public class ShiftManager : Singleton<ShiftManager>
     public void Penalize(int amount)
     {
         approvalScore = approvalScore - amount;
+        OnCustomerServed();
         Debug.Log($"Penalty: -{amount}");
     }
 
     public void Reward(int amount)
     {
         approvalScore = approvalScore + amount;
+        OnCustomerServed();
         Debug.Log($"Reward: +{amount}");
     }
 }
